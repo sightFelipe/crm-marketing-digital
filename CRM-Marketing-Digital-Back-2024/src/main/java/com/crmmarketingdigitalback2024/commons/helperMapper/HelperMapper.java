897f0033174a19fa21904e4e42eaa7bae0281540
jhoncastro1 <jhoncastro2004eg@gmail.com>
2024-02-26 21:06:48 -0500

    ABC - 04 - Configure the structure of the BE

---
+++ CRM-Marketing-Digital-Back-2024/src/main/java/com/crmmarketingdigitalback2024/commons/helperMapper/HelperMapper.java
@@ -0,0 +1,10 @@
+package com.crmmarketingdigitalback2024.commons.helperMapper;
+
+import org.modelmapper.ModelMapper;
+
+public class HelperMapper {
+    public static ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
