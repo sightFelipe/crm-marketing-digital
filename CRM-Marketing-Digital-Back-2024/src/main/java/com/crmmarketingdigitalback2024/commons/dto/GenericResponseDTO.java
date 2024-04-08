c9deb4a515a417ac445820be27266645e877bab7
FelipeOzaeta07 <anikilo12341@gmail.com>
2024-04-08 14:57:09 -0500

    VG012- [AUTHENTICATION]: REGISTER - BACKEND

---
+++ CRM-Marketing-Digital-Back-2024/src/main/java/com/crmmarketingdigitalback2024/commons/dto/GenericResponseDTO.java
@@ -0,0 +1,19 @@
+package com.crmmarketingdigitalback2024.commons.dto;
+
+import lombok.*;
+import org.aspectj.internal.lang.annotation.ajcDeclareEoW;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class GenericResponseDTO implements Serializable {
+    public String message;
+    public Object objectResponse;
+    public int objectId;
+    public int statusCode;
+
+}
